dc5296e5d43ef207c2b941de10d224f815d076aa
bekab <bereket5042@gmail.com>
2021-04-27 21:13:17 +0300

    Add solution 13 pr 0x13

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
-last_digit = (last_digit - 10) if (number < 0 and last_digit != 0) else last_digit
+last_digit = (last_digit - 10) if (number < 0 and
+                                   last_digit != 0) else last_digit
 if last_digit > 5:
     print("Last digit of {} is {} and {}".format(number, last_digit,
           "is greater than 5"))
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - will insert node to a linked list
+ * @head: head of a linked list
+ * @number: the new value
+ *
+ * Return: address of new node list
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *temp_node = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = number;
+	new_node->next = NULL;
+	if (head == NULL)
+	{
+		temp_node = new_node;
+		return (new_node);
+	}
+
+	while (temp_node != NULL)
+	{
+		if ((temp_node)->n > number)
+		{
+			new_node->next = temp_node;
+			temp_node = new_node;
+			return (new_node);
+		} else if ((temp_node)->next == NULL)
+		{
+			(temp_node)->next = new_node;
+			return (new_node);
+		} else if ((temp_node)->next->n > number)
+		{
+			new_node->next = (temp_node)->next;
+			(temp_node)->next = new_node;
+			return (new_node);
+		}
+		temp_node = (temp_node)->next;
+	}
+	return (NULL);
+}
