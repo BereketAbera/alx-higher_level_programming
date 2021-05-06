6c943006b0dded0c1098b9ef2916f0ed857e6b73
bekab <bereket5042@gmail.com>
2021-05-06 23:13:57 +0300

    Add solution 12 pr 0x04

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_chars = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                   'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str):
+        return None
+    val = 0
+    for c in roman_string:
+        if c not in roman_chars:
+            return None
+        val += roman_chars[c]
+    return val
