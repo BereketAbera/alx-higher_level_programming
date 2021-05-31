597e618ff692bbcbb303d500d9390ab1c4754e26
bekab <bereket5042@gmail.com>
2021-05-31 22:34:44 +0300

    Fis solution 2 pr 0x0A

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,6 @@
 def is_same_class(obj, a_class):
     """check exact type"""
 
-    if (isinstance(obj, a_class) and not issubclass(a_class, type(obj))):
+    if (type(obj) is a_class):
         return True
     return False
